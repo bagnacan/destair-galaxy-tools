f96a7ead2d2db6cdf3cc9b5b1655dcdc7851fee7
koriege <koriege@gmail.com>
2017-10-05 17:16:46 +0200

    adapted setup

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # tools
 1) export GALAXY_ROOT=/path/to/galaxy
 2) git clone https://github.com/galaxyproject/galaxy.git $GALAXY_ROOT
-3) once run $GALAXY_ROOT/run.sh
-4) run setup.sh
+3) ./setup.sh
--- setup.sh
+++ setup.sh
@@ -2,12 +2,16 @@
 
 dir=$(cd $(dirname $0) && pwd)
 for i in $(ls -d $dir/*/); do
-	ln -sfn $dir $GALAXY_ROOT/tools/$(basename $i)
-	ln -sfn $dir $GALAXY_ROOT/test-data/$(basename $i)
+	ln -sfn $i $GALAXY_ROOT/tools/$(basename $i)
+	ln -sfn $i $GALAXY_ROOT/test-data/$(basename $i)
 done
 
-source $GALAXY_ROOT/.venv/bin/activate
-pip install planemo bioblend
-#planemo test --galaxy_root=$GALAXY_ROOT
-planemo serve --galaxy_root=$GALAXY_ROOT
-deactivate
+[[ ! -e $GALAXY_ROOT/config/galaxy.ini ]] && cp $GALAXY_ROOT/config/galaxy.ini.sample $GALAXY_ROOT/config/galaxy.ini
+sed -i -r 's/^\s*#+(\s*tool_path.+)/\1/' $GALAXY_ROOT/config/galaxy.ini
+
+$GALAXY_ROOT/run.sh
+# source $GALAXY_ROOT/.venv/bin/activate
+# pip install planemo bioblend
+# planemo test --galaxy_root=$GALAXY_ROOT
+# planemo serve --galaxy_root=$GALAXY_ROOT
+# deactivate
